95569d3fec0483c2efce01fa656b733cacacb726
Avatarfreak <avatarfreak@users.noreply.github.com>
2019-06-29 23:05:24 +0530

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+const checkbox = document.getElementById("nav-toggle");
+const nav = document.getElementById("navbar");
+
+nav.addEventListener("click", () => {
+
+    checkbox.checked = true;
+});
+
+function myFunction() {}
